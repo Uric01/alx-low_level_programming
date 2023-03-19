68982a9f37cc030d8f5bad0b9caa14ac2cb65811
Uric01 <uric01@live.com>
2023-03-19 04:20:55 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%i is negative", n);
+printf(" % i is negative"\n, n);
 }
 else if (n > -1 && n != 0)
 {
-printf("%i is positive", n);
+printf(" % i is positive"\n", n);
 }
 else if (n == 0)
 {
-printf("%i is zero", n);
+printf(" % i is zero"\n, n);
 }
 return (0);
 }
