d4e5868247600bde108019077af00b0bac5f0643
Uric01 <uric01@live.com>
2023-03-24 17:51:26 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - check the code
- *
  * Return: Always 0 if successful
  */
 int main()
@@ -10,15 +9,15 @@ int main()
     
     for (i = 1;  i <= 100; i++)
     {
-        if(i % 3 == 0)
+        if (i % 3 == 0)
         {
             printf(" Fizz ");
         }
-        else if(i % 5 == 0)
+        else if (i % 5 == 0)
         {
             printf(" Buzz ");
         }
-        else if((i % 3 == 0) && (i % 5 == 0))
+        else if ((i % 3 == 0) && (i % 5 == 0))
         {
              printf(" FizzBuzz ");
         }
