779e95f17d50c3a8274c79b09928663bea86df4c
Uric01 <uric01@live.com>
2023-03-24 17:57:50 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,28 +5,28 @@
  */
 int main()
 {
-    int i;
-    
-    for (i = 1;  i <= 100; i++)
-    {
+	int i;
+
+	for (i = 1;  i <= 100; i++)
+	{
         if (i % 3 == 0)
         {
-            printf(" Fizz ");
+	printf(" Fizz ");
         }
         else if (i % 5 == 0)
         {
-            printf(" Buzz ");
+	printf(" Buzz ");
         }
         else if ((i % 3 == 0) && (i % 5 == 0))
         {
-             printf(" FizzBuzz ");
+	printf(" FizzBuzz ");
         }
         else
         {
-            printf(" %d", i);
+	printf(" %d", i);
         }
-    }
-    printf("\n");
+	}
+	printf("\n");
 
-    return 0;
+	return 0;
 }
